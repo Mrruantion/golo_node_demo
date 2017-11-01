2bad6e54d269fe232932a40d28e6e4355413d66a
mrruantion <18344178461@163.com>
2017-11-01 17:55:35 +0800

    add golo message

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cookieParser('demo'));
-app.use(session({ secret: 'demo', resave: true, saveUninitialized: true }));
-
+// app.use(cookieParser('demo'));
+// app.use(session({ secret: 'demo', resave: true, saveUninitialized: true }));
+console.log(index,'index')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  // console.log(req)
+  res.render('index', {title:'文章列表'});
 });
 
+router.get('/index',function(req,res,next){
+  res.render('index',{})
+})
 /* GET admin page. */
 router.get('/admin', function(req, res, next) {
-    res.render('admin', {})
+    res.render('admin', {title:'title'})
 });
 
 module.exports = router;
